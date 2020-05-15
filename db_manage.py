c8de5e55c97d3dc4e745302ffe5a7033150409ee
Chaitanya Dodiya <chaitanya_dodiya>
2020-05-15 14:23:55 +0530

    create, terminate and excute query added

--- db_manage.py
+++ db_manage.py
@@ -5,3 +5,14 @@ class Database:
 	def __init__(self):
 		self.conn = None
 		self.cursor = None
+
+	def make_connection(self):
+		self.conn = sqlite3.connect('temples.db')
+		self.cursor = self.conn.cursor()
+
+	def close_connection():
+		self.conn.close()
+
+	def execute_query(self, query):
+		self.cursor.execute(*query)
+		self.conn.commit()
