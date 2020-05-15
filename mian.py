9154304e3adbc6d1dbce66001e00b73425af57d8
Chaitanya Dodiya <chaitanya_dodiya>
2020-05-15 14:08:20 +0530

    db_manage file initilized

--- mian.py
+++ mian.py
@@ -1,8 +1,16 @@
 import requests
 
 
+def get_temple_links():
+	pass
+
 def main():
-	
+	'''
+	So There is a show more button on the website so could not get it without selenium.
+	I copied code of that and put that into abc.html file and will read it from it.
+	'''
+
+	templeLinks = get_temple_links()
 
 if __name__ == '__main__':
 	main()
